77a1ebf220c067998dbb8a8b4bd4e8f18ef18b80
Alex <89443340+kansasdynamics@users.noreply.github.com>
2023-11-16 08:07:28 -0800

    Update hashcat_helper.sh

--- hashcat_helper.sh
+++ hashcat_helper.sh
@@ -2,3 +2,10 @@
 
 # Check to see if nmap is installed
 # If not installed, then install
+if [ $(dpkg-query -W -f='${Status}' hashcat 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  echo "The required packages are not installed. Installing required packages now."
+  nmap;
+  apt install hashcat -y
+  echo "Required packages have successfully been installed."
+fi
