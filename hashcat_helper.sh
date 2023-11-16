38cb3a8feade5b09c87c071eb8cf495318039160
Alex <89443340+kansasdynamics@users.noreply.github.com>
2023-11-16 08:08:28 -0800

    Update hashcat_helper.sh

--- hashcat_helper.sh
+++ hashcat_helper.sh
@@ -5,7 +5,6 @@
 if [ $(dpkg-query -W -f='${Status}' hashcat 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
   echo "The required packages are not installed. Installing required packages now."
-  nmap;
   apt install hashcat -y
   echo "Required packages have successfully been installed."
 fi
